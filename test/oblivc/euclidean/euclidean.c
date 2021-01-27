cd04dad324ed1ea544bbf3dd32b384c43ac116d6
Mitchell Negus <mitchell.negus.57@gmail.com>
2021-01-27 13:41:43 -0800

    working euclidean distance with an arbitrary number of vector elements

--- test/oblivc/euclidean/euclidean.c
+++ test/oblivc/euclidean/euclidean.c
@@ -1,42 +1,127 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<obliv.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <obliv.h>
 
-#include"euclidean.h"
-#include"../common/util.h"
+#include "dbg.h"
+#include "euclidean.h"
+#include "../common/util.h"
 
 
-int main(int argc, char *argv[])
-{
+void check_mem(int *x1, int *x2, int party) {
+  if (party == 1) {
+    if (x1 == NULL) {
+      log_err("Memory allocation failed\n");
+      clean_errno();
+      exit(1);
+    }
+  }
+  if (party == 2) {
+    if (x2 == NULL) {
+      log_err("Memory allocation failed\n");
+      clean_errno();
+      exit(1);
+    }
+  }
+}
+
+
+int main(int argc, char *argv[]) {
   ProtocolDesc pd;
   protocolIO io;
-  if (argc < 6) {
-    if (argc <2 ) fprintf(stderr,"Port number missing\n");
-    else if (argc <3 ) fprintf(stderr,"Protocol ID missing\n");
-    else if (argc <4 ) fprintf(stderr,"Remote server missing\n");
-    else fprintf(stderr,"Vector elements missing\n");
-    fprintf(stderr,"Usage: %s <port> <proto> <--|server> <string>\n", argv[0]);
-    return 1;
-  }
 
-  io.x = atoi(argv[4]);
-  io.y = atoi(argv[5]);
-  if (io.x > MAXN || io.y > MAXN) {
-    fprintf(stderr,"Vector element too big\n");
-    return 1;
-  }
+  printf("Euclidean Distance Calculation\n");
+  printf("——————————————————————————————\n\n");
 
-  //protocolUseStdio(&pd);
-  const char* remote_host = (strcmp(argv[3], "--") == 0 ? NULL: argv[3]);
-  ocTestUtilTcpOrDie(&pd, remote_host,argv[1]);
+  // Check arguments
+  if (argc == 4) {
+    // Initialize protocols and obtain connection information
+    const char *remote_host = strtok(argv[1], ":");
+    const char *port = strtok(NULL, ":");
 
-  setCurrentParty(&pd, (remote_host ? 2: 1));
-  if (!strcmp("yao",argv[2])) {
+    // Make connection between two shells
+    // Modified ocTestUtilTcpOrDie() function from test/oblivc/common/util.c
+    log_info("Connecting to %s on port %s ...\n", remote_host, port);
+    if (argv[2][0] == '1') {
+      if (protocolAcceptTcp2P(&pd, port) != 0) {
+        log_err("TCP accept from %s failed\n", remote_host);
+        exit(1);
+      }
+    } else {
+      if(protocolConnectTcp2P(&pd, remote_host, port) != 0) {
+        log_err("TCP connect to %s failed\n", remote_host);
+        exit(1);
+      }
+    }
+
+    // Final initializations before entering protocol
+    int current_party = atoi(argv[2]);
+    if (current_party != 1 && current_party != 2) {
+      log_err("Party number must be either 1 or 2\n");
+      exit(1);
+    }
+    setCurrentParty(&pd, current_party);
+    io.src = argv[3];
+    double lap = wallClock();
+
+    // Execute Yao's protocol and cleanup
     execYaoProtocol(&pd, euclideanDistance, &io);
+    cleanupProtocol(&pd);
+    // Stop the clock
+    double runtime = wallClock() - lap;
+
+    // Print results and gate count
+    log_info("Total time: %lf seconds\n", runtime);
+    // log_info("Yao Gate Count: %u\n", yaoGateCount());
+    printf("\n");
+    log_info("Squared distance \td = %i\n", io.dist);
   } else {
-    execNpProtocol_Bcast1(&pd, euclideanDistance, &io);
+    log_info("Usage: %s <hostname:port> <1|2> <filename>\n"
+             "\tHostname usage:\n"
+             "\tlocal -> 'localhost' remote -> IP address or DNS name\n",
+             argv[0]);
   }
-  fprintf(stderr,"Result: %d\n", io.res);
-  cleanupProtocol(&pd);
   return 0;
+  }
+
+
+void load_data(protocolIO *io, int **x1, int **x2, int party) {
+  FILE *inputFile = fopen(io->src, "r");
+  if (inputFile == NULL) {
+    log_err("File '%s' not found\n", io->src);
+    clean_errno();
+    exit(1);
+  }
+
+  io->n = 0;
+  int memsize = ALLOC;
+  int element;
+  while (!feof(inputFile)) {
+    // Read the integer from each line in the file
+    int dataPoints = fscanf(inputFile, "%d", &element);
+    if ( dataPoints != 1) {
+      if (feof(inputFile))
+        break;
+      log_err("Input from '%s' does not match file format.\n", io->src);
+      clean_errno();
+      exit(1);
+    }
+
+    io->n += 1;
+    if (io->n > memsize) {
+      memsize *= 2;
+      *x1 = realloc(*x1, sizeof(int) * memsize);
+      *x2 = realloc(*x2, sizeof(int) * memsize);
+      check_mem(*x1, *x2, party);
+    }
+
+    int adj = io->n - 1;
+    if (party == 1)
+      *(*x1 + adj) = element;
+    else if (party == 2)
+      *(*x2 + adj) = element;
+  }
+
+  log_info("Loaded %d data points\n", io->n);
+  fclose(inputFile);
 }
--- test/oblivc/euclidean/euclidean.h
+++ test/oblivc/euclidean/euclidean.h
@@ -1,10 +1,12 @@
 #pragma once
 
-#define MAXN 100
+#define ALLOC 128
 
 typedef struct protocolIO {
-  int x, y;
-  int res;
+  char *src; // Filename for data
+  int n;     // Number of vector elements
+  int dist;  // Distance
 } protocolIO;
 
 void euclideanDistance(void* args);
+void load_data(protocolIO *io, int **x1, int **x2, int party);
